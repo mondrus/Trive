bf10ccd940447129a5616193b1fee05c770f8d73
mondrus <david@mondrus.com>
2017-05-21 10:58:17 -0400

    Intermediate step of installing INSIPIA Template

---
+++ client/config/router.js
@@ -0,0 +1,31 @@
+Router.configure({
+    layoutTemplate: 'mainLayout',
+    notFoundTemplate: 'notFound'
+
+});
+
+
+//
+// Landing page
+//
+/*
+Router.route('/landing', function () {
+    this.render('landing');
+    this.layout('blankLayout')
+});
+
+//
+// Other pages routes
+//
+Router.route('/notFound', function () {
+    this.render('notFound');
+});
+
+// Default route
+// You can use direct this.render('template')
+// We use Router.go method because dashboard1 is our nested view in menu
+Router.route('/', function () {
+    Router.go('dashboard1');
+});
+
+*/
